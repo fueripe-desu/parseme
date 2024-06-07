b82394dfe32b39877db9ec3dee1d30ec0a76019a
fueripe-desu <fueripedesu2020@outlook.com>
2024-06-07 02:02:48 -0300

    feat: add new errors for Property and Stack

--- errors.go
+++ errors.go
@@ -41,5 +41,56 @@ func (e *FileNotReadable) Error() string {
 type ReadNegativeSizeError struct{}
 
 func (e *ReadNegativeSizeError) Error() string {
-	return "Cannot read file if scale is negative:"
+	return "Cannot read file if scale is negative."
+}
+
+type StackError struct {
+	message string
+}
+
+func (e *StackError) Error() string {
+	return e.message
+}
+
+// Property error
+type PropertyError struct {
+	Message string
+}
+
+func (e *PropertyError) Error() string {
+	return e.Message
+}
+
+type PropertyBooleanValueError struct{}
+
+func (e *PropertyBooleanValueError) Error() string {
+	err := PropertyError{Message: "Cannot get boolean value of non-boolean elements."}
+	return err.Error()
+}
+
+type PropertyInvalidNameError struct {
+	name string
+}
+
+func (e *PropertyInvalidNameError) Error() string {
+	msg := fmt.Sprintf("The property name '%v' is not valid.", e.name)
+	err := PropertyError{Message: msg}
+	return err.Error()
+}
+
+type PropertyEmptyNameError struct{}
+
+func (e *PropertyEmptyNameError) Error() string {
+	err := PropertyError{Message: "Name must not be empty."}
+	return err.Error()
+}
+
+type PropertyInvalidBooleanError struct {
+	value string
+}
+
+func (e *PropertyInvalidBooleanError) Error() string {
+	msg := fmt.Sprintf("Boolean property value must be either 'true' or 'false'. Instead got: '%v'", e.value)
+	err := PropertyError{Message: msg}
+	return err.Error()
 }
