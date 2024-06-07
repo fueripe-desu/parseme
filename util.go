df491bce0295ae933788c9e7152d2c839e06cf8e
fueripe-desu <fueripedesu2020@outlook.com>
2024-06-07 01:59:49 -0300

    feat: add util functions to help with string validation

---
+++ util.go
@@ -0,0 +1,69 @@
+package parseme
+
+import "regexp"
+
+func isValidIdentifier(value string) bool {
+	isValid := hasValidIdentifierChars(value)
+	firstChar := firstCharIsDigit(value)
+	return isValid && (!firstChar)
+}
+
+func isValidProperty(value string) bool {
+	isValid := hasValidPropertyChars(value)
+	firstChar := firstCharIsDigit(value)
+	return isValid && (!firstChar)
+}
+
+func hasValidPropertyChars(value string) bool {
+	if len(value) == 0 {
+		return false
+	}
+
+	pattern := `^[a-zA-Z0-9\-\_]*$`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(value)
+}
+
+func hasValidIdentifierChars(value string) bool {
+	if len(value) == 0 {
+		return false
+	}
+
+	pattern := `^[a-zA-Z0-9\-\:\.\_]*$`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(value)
+}
+
+func firstCharIsDigit(value string) bool {
+	if len(value) == 0 {
+		return false
+	}
+
+	pattern := `[0-9]`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(string(value[0]))
+}
+
+func isDoubleQuoted(value string) bool {
+	if len(value) < 2 {
+		return false
+	}
+
+	return value[0] == '"' && value[len(value)-1] == '"'
+}
+
+func isSingleQuoted(value string) bool {
+	if len(value) < 2 {
+		return false
+	}
+
+	return value[0] == '\'' && value[len(value)-1] == '\''
+}
+
+func removeQuotes(value string) string {
+	if isSingleQuoted(value) || isDoubleQuoted(value) {
+		return value[1 : len(value)-1]
+	}
+
+	return value
+}
