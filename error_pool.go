b58ec3d1b9ef4cb14ab6d5c4684899ba94e27cb8
fueripe-desu <fueripedesu2020@outlook.com>
2024-06-14 14:03:31 -0300

    feat: add masked errors in other to pass dynamic values to error pool

--- error_pool.go
+++ error_pool.go
@@ -1,5 +1,9 @@
 package parseme
 
+import (
+	"strconv"
+)
+
 type ErrorLevel int
 
 const (
@@ -61,13 +65,82 @@ func (p *ErrorPool) UnsubscribeAll() {
 	p.observers = p.observers[:0]
 }
 
-func (p *ErrorPool) Error(data ErrorData) {
-	p.errorStack.Push(data)
+func (p *ErrorPool) Error(data ErrorData, args []string) {
+	p.AddError(data, args)
 	p.Notify()
 }
 
-func (p *ErrorPool) AddError(data ErrorData) {
-	p.errorStack.Push(data)
+func (p *ErrorPool) AddError(data ErrorData, args []string) {
+	var finalData ErrorData
+	if args != nil && len(args) > 0 {
+		finalData = p.precompileError(data, args)
+	} else {
+		finalData = data
+	}
+
+	p.errorStack.Push(finalData)
+}
+
+func (p *ErrorPool) precompileError(data ErrorData, args []string) ErrorData {
+	newMsg := p.replaceMasks(data.message, args)
+	return ErrorData{name: data.name, level: data.level, message: newMsg}
+}
+
+func (p *ErrorPool) replaceMasks(input string, args []string) string {
+	chars := []byte(input)
+	buffer := []byte{}
+
+	for i := 0; i < len(chars); i++ {
+		current := chars[i]
+
+		if next := i + 1; current == '%' && next < len(chars) && chars[next] == '{' {
+			newIndex, argIndex := p.consumeMask(i+2, &chars)
+			i = newIndex
+
+			if argIndex == -1 {
+				continue
+			}
+
+			if argIndex >= len(args) {
+				continue
+			}
+
+			argBytes := []byte(args[argIndex])
+			buffer = append(buffer, argBytes...)
+		} else {
+			buffer = append(buffer, current)
+		}
+	}
+
+	return string(buffer)
+}
+
+func (p *ErrorPool) consumeMask(start int, chars *[]byte) (int, int) {
+	buffer := []byte{}
+
+	startIndex := start
+	for ; startIndex < len(*chars); startIndex++ {
+		current := (*chars)[startIndex]
+
+		if current == '}' {
+			break
+		}
+
+		if current >= '0' && current <= '9' {
+			buffer = append(buffer, (*chars)[startIndex])
+		}
+	}
+
+	var argsIndex int
+
+	if len(buffer) == 0 {
+		argsIndex = -1
+	} else {
+		intIndex, _ := strconv.Atoi(string(buffer))
+		argsIndex = intIndex
+	}
+
+	return startIndex, argsIndex
 }
 
 func (p *ErrorPool) Notify() error {
