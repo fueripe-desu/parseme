80913642d52d9c55f091dd7f10b3e72df44a357e
fueripe-desu <fueripedesu2020@outlook.com>
2024-06-13 23:27:08 -0300

    feat: add errors for error pool

---
+++ error_pool_errors.go
@@ -0,0 +1,19 @@
+package parseme
+
+type ObserverNotFoundError struct{}
+
+func (e *ObserverNotFoundError) Error() string {
+	return "Observer does not exist."
+}
+
+type ObserverDuplicateError struct{}
+
+func (e *ObserverDuplicateError) Error() string {
+	return "Observer already exists."
+}
+
+type NotifyObserverError struct{}
+
+func (e *NotifyObserverError) Error() string {
+	return "Cannot notify when there are no errors."
+}
