a8db6907609c33e752442670cf76349921167994
fueripe-desu <fueripedesu2020@outlook.com>
2024-06-16 16:52:08 -0300

    feat: remove properties from error data and make them dynamic

--- error_pool.go
+++ error_pool.go
@@ -1,7 +1,12 @@
 package parseme
 
 import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -9,6 +14,7 @@ type ErrorLevel int
 
 const (
 	Fatal ErrorLevel = iota
+	Error
 	Warning
 	Info
 	Debug
@@ -16,26 +22,32 @@ const (
 )
 
 type errorData struct {
-	name       string
-	message    string
-	code       string
-	caller     string
-	module     string
-	fix        string
-	lineNumber int
-	level      ErrorLevel
+	name    string
+	message string
+	code    string
+	module  string
+	fix     string
+	level   ErrorLevel
 }
 
 type ErrorInfo struct {
-	Name       string
-	Message    string
-	Code       string
-	Caller     string
-	Module     string
-	Fix        string
-	LineNumber int
-	Level      ErrorLevel
-	Timestamp  time.Time
+	Name    string
+	Message string
+	Code    string
+	Module  string
+	Fix     string
+
+	CallerFuncName string
+	CallerFilename string
+	CallerLine     int
+
+	ErrorFuncName string
+	ErrorFilename string
+	ErrorLine     int
+	ErrorSite     string
+
+	Level     ErrorLevel
+	Timestamp time.Time
 }
 
 type ErrorPool struct {
@@ -83,9 +95,13 @@ func (p *ErrorPool) UnsubscribeAll() {
 	p.observers = p.observers[:0]
 }
 
-func (p *ErrorPool) Error(data errorData, args []string) {
-	p.AddError(data, args)
-	p.Notify()
+func (p *ErrorPool) Error(data *errorData, args []string) {
+	if data == nil {
+		p.Error(nilErrorDataError, nil)
+	} else {
+		p.AddError(*data, args)
+		p.Notify()
+	}
 }
 
 func (p *ErrorPool) AddError(data errorData, args []string) {
@@ -168,22 +184,99 @@ func (p *ErrorPool) Notify() error {
 
 	last := p.errorStack.Peek()
 
+	recursive := false
+	if last.name == "Nil error data" {
+		recursive = true
+	}
+
+	callerLine, callerFile, callerFuncName, _ := p.getErrorInfo(recursive, true)
+	errorLine, errorFile, errorFuncName, errorSite := p.getErrorInfo(recursive, false)
+
 	for _, o := range p.observers {
 		info := ErrorInfo{
-			last.name,
-			last.message,
-			last.code,
-			last.caller,
-			last.module,
-			last.fix,
-			last.lineNumber,
-			last.level,
-			time.Now(),
+			Name:           last.name,
+			Message:        last.message,
+			Code:           last.code,
+			Module:         last.module,
+			Fix:            last.fix,
+			CallerFuncName: callerFuncName,
+			CallerFilename: callerFile,
+			CallerLine:     callerLine,
+			ErrorFuncName:  errorFuncName,
+			ErrorFilename:  errorFile,
+			ErrorLine:      errorLine,
+			ErrorSite:      errorSite,
+			Level:          last.level,
+			Timestamp:      time.Now(),
 		}
 		o.OnUpdate(info)
 	}
 
 	return nil
+}
+
+func (p *ErrorPool) getErrorInfo(recursive bool, caller bool) (int, string, string, string) {
+	index := p.getStackIndex(recursive, caller)
+
+	pc, file, line, ok := runtime.Caller(index)
+	funcName := p.extractFuncName(runtime.FuncForPC(pc).Name())
+
+	if !ok {
+		panic("Could not retrieve runtime caller.")
+	}
+
+	return line, filepath.Base(file), funcName, p.getLineContents(file, line)
+}
+
+func (p *ErrorPool) getStackIndex(recursive bool, caller bool) int {
+	if recursive {
+		if caller {
+			return 3
+		}
+
+		return 4
+	} else {
+		if caller {
+			return 2
+		}
+
+		return 3
+	}
+}
+
+func (p *ErrorPool) extractFuncName(fullName string) string {
+	splitted := strings.Split(fullName, "/")
+	last := splitted[len(splitted)-1]
+	pos := strings.Index(last, ".")
+	return last[pos+1:]
+}
+
+func (p *ErrorPool) getLineContents(filepath string, line int) string {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		panic("Could not open file.")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	currentLine := 1
+
+	for scanner.Scan() {
+		if currentLine == line {
+			withoutControl := removeControlChars(scanner.Text())
+			return strings.Trim(withoutControl, " ")
+		}
+
+		currentLine++
+	}
+
+	if scannerErr := scanner.Err(); scannerErr != nil {
+		panic("Scanner could not read the file.")
+	}
+
+	panic("File line does not exist.")
 }
 
 func (p *ErrorPool) ClearErrors() {
@@ -198,20 +291,16 @@ func NewErrorData(
 	name string,
 	message string,
 	code string,
-	caller string,
 	module string,
 	fix string,
-	lineNumber int,
 	level ErrorLevel,
 ) *errorData {
 	return &errorData{
-		name:       name,
-		message:    message,
-		code:       code,
-		caller:     caller,
-		module:     module,
-		fix:        fix,
-		lineNumber: lineNumber,
-		level:      level,
+		name:    name,
+		message: message,
+		code:    code,
+		module:  module,
+		fix:     fix,
+		level:   level,
 	}
 }
--- error_pool_errors.go
+++ error_pool_errors.go
@@ -17,3 +17,12 @@ type NotifyObserverError struct{}
 func (e *NotifyObserverError) Error() string {
 	return "Cannot notify when there are no errors."
 }
+
+var nilErrorDataError *errorData = NewErrorData(
+	"Nil error data",
+	"Error data must not be nil.",
+	"ED1",
+	"Error Pool",
+	"Try handling nil error data pointers before passing it to the function, or assing a new address to error data.",
+	Error,
+)
